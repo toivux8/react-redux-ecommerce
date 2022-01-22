de760bf020df9a40ec3ea31517b7f2dd7574b357
Toi Vũ <58621888+toivux8@users.noreply.github.com>
2022-01-22 20:41:44 +0700

    fix display products

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import formatCurrency from '../util';
 
 export default class Products extends Component {
   render() {
@@ -12,7 +13,7 @@ export default class Products extends Component {
                             <p>{product.title}</p>
                         </a>
                         <div className='product-price'> 
-                            <div>{product.price}</div>
+                            <div>{formatCurrency(product.price)}</div>
                             <button className='button primary'>Add To Cart</button>
                         </div>
                     </div>
