69efc973dcd512a8fea61ea53cbefdbbbb43cef5
Toi Vũ <58621888+toivux8@users.noreply.github.com>
2022-01-22 13:50:15 +0700

    Add products list

---
+++ src/components/Products.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+export default class Products extends Component {
+  render() {
+    return <div>
+        <ul className='products'>
+            {this.props.products.map(product =>(
+                <li key={product._id}>
+                    <div className='product'>
+                        <a href={"#" + product._id}>
+                            <img src={product.image} alt={product.title}></img>
+                            <p>{product.title}</p>
+                        </a>
+                        <div className='product-price'> 
+                            <div>{product.price}</div>
+                            <button className='button primary'>Add To Cart</button>
+                        </div>
+                    </div>
+                </li>
+            ))}
+        </ul>
+    </div>;
+  }
+}
