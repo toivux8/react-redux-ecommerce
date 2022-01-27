643aa0da7ea550b5266842eecabf570354a6e36a
Toi Vũ <58621888+toivux8@users.noreply.github.com>
2022-01-27 16:16:55 +0700

    add-number-dress

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import formatCurrency from '../util';
 
 export default class Cart extends Component {
   render() {
@@ -6,9 +7,35 @@ export default class Cart extends Component {
     const {cartItems} = this.props
 
     return (
+      <div>
         <div>
-            { cartItems === 0 ? <div className='cart cart-header'>Cart is empty</div> : <div className='cart cart-header'>You have {cartItems.length} in the cart</div>}
+            { cartItems.length === 0 ? <div className='cart cart-header'>Cart is empty</div> : <div className='cart cart-header'>You have {cartItems.length} item(s) in the cart.</div>}
         </div>
+        <div className='cart'>
+          <ul className='cart-items'>
+            {cartItems.map((item) => (
+              <li key={item._id}>
+                <div>
+                  <img src={item.image} alt={item.title}></img>
+                </div>
+                <div>
+                  <div>{item.title}</div>
+                  <div className='right-btn'>
+                    {formatCurrency(item.price)} x {item.count}{" "}
+                    <button className='button' onClick={() => this.props.removeFromCart(item)}>
+                      Remove
+                    </button>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div>
+          {/* Your bill is {totalBill} $ */}
+        </div>
+      </div>
     )
   }
 }
